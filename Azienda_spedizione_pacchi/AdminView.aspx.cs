fbf9f81cb2328c04ccbb40050b96657964ba5bc5
ImNotRacistBut <andrelomb2002@gmail.com>
2021-04-05 15:45:28 +0200

    >Aggiunta admin view e correlati

---
+++ Azienda_spedizione_pacchi/AdminView.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Azienda_spedizione_pacchi
+{
+    public partial class Amministratore : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
