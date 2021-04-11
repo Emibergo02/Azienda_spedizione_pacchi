01868494fa87717ad5a126ecb64dd0410cfec46a
Emibergo02 <emibergo@gmail.com>
2021-04-11 15:25:23 +0200

    Commit della domenica pomeriggio

--- Azienda_spedizione_pacchi/UserView.aspx.cs
+++ Azienda_spedizione_pacchi/UserView.aspx.cs
@@ -2,10 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Azienda_spedizione_pacchi
 {
@@ -13,25 +10,28 @@ namespace Azienda_spedizione_pacchi
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Cliente c = null;
             if (!Page.IsPostBack)
                 if (Session["clienteLoggato"] != null)
                 {
-                    Cliente c = (Cliente)Session["clienteLoggato"];
-                    if (c.Utente.Privilegi != 1) Response.Redirect("LogIn.aspx");
-
-                    List<Pacco> listaPacchi = DataAccess.OttieniListaPacchi(ConfigurationManager.ConnectionStrings["ConnectionStringAziendaSpedizionePacchiMySQL"].ConnectionString);
-                    List<Pacco> listaPacchiCliente = new List<Pacco>();
-                    foreach(Pacco p in listaPacchi)
+                    c = (Cliente)Session["clienteLoggato"];
+                    if (c.Utente.Privilegi != 1)
                     {
-                        if(p.Destinatario!=null)
-                        if (p.Destinatario.IdCliente == c.IdCliente)
-                            listaPacchiCliente.Add(p);
+                        Response.Redirect("LogIn.aspx");
+                        return;
                     }
-                    
-                    rptViaggi.DataSource = listaPacchiCliente;
-                    rptViaggi.DataBind();
                 }
-                else Response.Redirect("LogIn.aspx");
+                else
+                {
+                    Response.Redirect("LogIn.aspx");
+                    return;
+                }
+
+            List<Pacco> listaPacchi = DataAccess.OttieniListaPacchi(ConfigurationManager.ConnectionStrings["ConnectionStringAziendaSpedizionePacchiMySQL"].ConnectionString,c.IdCliente);
+
+
+            rptViaggi.DataSource = listaPacchi;
+            rptViaggi.DataBind();
 
         }
     }
